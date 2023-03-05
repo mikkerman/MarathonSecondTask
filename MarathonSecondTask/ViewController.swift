7b79d31bc987401e6ab32ef85ae0e1f6230ed492
Mikhail German <mikhail.german@icloud.com>
2023-03-05 13:29:02 +0300

    Initial Commit

---
+++ MarathonSecondTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarathonSecondTask
+//
+//  Created by Михаил Герман on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
